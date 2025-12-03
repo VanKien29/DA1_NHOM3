34b1017dec596c9f73ebd3a4f85025cf627d0dda
BCHoan <183281071+BCHoan@users.noreply.github.com>
2025-12-03 17:01:20 +0700

    fix_lỗi

--- views/Hotel/UpdateHotel.php
+++ views/Hotel/UpdateHotel.php
@@ -2,9 +2,9 @@
     <div class="admin-form-card">
         <h4 class="form-title text-center mb-4">Cập Nhật Hotel</h4>
 
-        <form method="POST">
+        <form method="POST" enctype="multipart/form-data">
             <?php if (!empty($err['empty'])): ?>
-            <div class="text-danger err"><?= $err['empty']; ?></div>
+                <div class="text-danger err"><?= $err['empty']; ?></div>
             <?php endif; ?>
 
             <!-- Tên dịch vụ / hotel -->
@@ -13,7 +13,23 @@
                 <input type="text" name="service_name" class="form-control"
                     value="<?= htmlspecialchars($hotel['service_name'] ?? '') ?>">
                 <?php if (!empty($err['service_name'])): ?>
-                <div class="text-danger err"><?= $err['service_name']; ?></div>
+                    <div class="text-danger err"><?= $err['service_name']; ?></div>
+                <?php endif; ?>
+            </div>
+            <div class="form-group mb-3">
+                <label>Chủ khách sạn</label>
+                <input type="text" name="hotel_manager" class="form-control"
+                    value="<?= htmlspecialchars($hotel['hotel_manager'] ?? '') ?>">
+                <?php if (!empty($err['hotel_manager'])): ?>
+                    <div class="text-danger err"><?= $err['hotel_manager']; ?></div>
+                <?php endif; ?>
+            </div>
+            <div class="form-group mb-3">
+                <label>Số điện thoại</label>
+                <input type="text" name="hotel_manager_phone" class="form-control"
+                    value="<?= htmlspecialchars($hotel['hotel_manager_phone'] ?? '') ?>">
+                <?php if (!empty($err['hotel_manager_phone'])): ?>
+                    <div class="text-danger err"><?= $err['hotel_manager_phone']; ?></div>
                 <?php endif; ?>
             </div>
 
@@ -23,7 +39,7 @@
                 <input type="text" name="room_type" class="form-control"
                     value="<?= htmlspecialchars($hotel['room_type'] ?? '') ?>">
                 <?php if (!empty($err['room_type'])): ?>
-                <div class="text-danger err"><?= $err['room_type']; ?></div>
+                    <div class="text-danger err"><?= $err['room_type']; ?></div>
                 <?php endif; ?>
             </div>
 
@@ -33,7 +49,7 @@
                 <input type="number" step="0.01" name="price_per_night" class="form-control"
                     value="<?= htmlspecialchars($hotel['price_per_night'] ?? '') ?>">
                 <?php if (!empty($err['price_per_night'])): ?>
-                <div class="text-danger err"><?= $err['price_per_night']; ?></div>
+                    <div class="text-danger err"><?= $err['price_per_night']; ?></div>
                 <?php endif; ?>
             </div>
 
@@ -43,14 +59,14 @@
                 <textarea name="description" class="form-control"
                     rows="3"><?= htmlspecialchars($hotel['description'] ?? '') ?></textarea>
                 <?php if (!empty($err['description'])): ?>
-                <div class="text-danger err"><?= $err['description']; ?></div>
+                    <div class="text-danger err"><?= $err['description']; ?></div>
                 <?php endif; ?>
             </div>
             <div class="mb-3">
                 <label>Ảnh Khách Sạn</label>
                 <input type="file" name="hotel_image" class="form-control">
-               <?php if (!empty($err['hotel_image'])): ?>
-                <div class="text-danger err"><?= htmlspecialchars($err['hotel_image']) ?></div>
+                <?php if (!empty($err['hotel_image'])): ?>
+                    <div class="text-danger err"><?= htmlspecialchars($err['hotel_image']) ?></div>
                 <?php endif; ?>
             </div>
 
